0a6839e64abd9d7937ab29cd972d4825cec1f09c
Troy Stribling <troy.stribling@gmail.com>
2025-06-01 21:05:02 -0500

    trying to get app server to load statuc files

--- api/main.py
+++ api/main.py
@@ -1,11 +1,17 @@
 # backend/main.py
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import os
 
 app = FastAPI()
 
 class RequestPayload(BaseModel):
     input: str
+
+html_path = os.path.join(os.path.dirname(__file__), "../html")
+app.mount("/", StaticFiles(directory=html_path, html=True), name="html")
 
 @app.post("/api/request")
 async def generate_markdown(req: RequestPayload):
