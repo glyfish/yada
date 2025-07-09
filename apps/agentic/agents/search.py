56e66648fdecf6bc6c7eea467319a2e487da5e7c
Troy Stribling <troy.stribling@gmail.com>
2025-07-08 22:01:14 -0500

    agent refactor works

--- apps/agentic/agents/search.py
+++ apps/agentic/agents/search.py
@@ -6,43 +6,23 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.prebuilt import ToolNode
 
 from apps.agentic.core.messages import WorkerState
+from apps.agentic.core.tool_agent import ToolAgent
 from apps.agentic.core.utils import build_llm, should_continue
 
 
-class SearchAgent:
+class SearchAgent(ToolAgent):
     """
     Search Agent that uses a language model to research and find relevant information.
     It can call tools like TavilySearchResults to fetch search results.
     """
 
     def __init__(self):
-        self.__llm = build_llm()
-        self.__prompt = self.__create_prompt()
-        self.__tools = [TavilySearchResults()]
-        self.__tool_node = ToolNode(self.__tools, name="researcher_tool_node")
-        self.__tooled_llm = self.__llm.bind_tools(self.__tools)
-        self.__agent = self.__create_agent()
-
-
-    @property
-    def agent(self):
-        """
-        Get the compiled agent state graph.
-        """
-        return self.__agent
+        tools = [TavilySearchResults()]
+        tool_node = ToolNode(tools, name="tavily_search_tool_node")
+        super().__init__(tools, tool_node)
     
-    async def __invoke_model(self, state: WorkerState, config=None) -> WorkerState:
-        """
-        Invoke the agent with the current state.
-        """
 
-        messages = state["messages"]
-        prompt_messages = self.__prompt.format_messages(messages=messages)
-        result = await self.__tooled_llm.ainvoke(prompt_messages)
-        return {"messages": [result]}
-
-    
-    def __create_prompt(self):
+    def create_prompt(self):
         """
         Create the prompt template for the researcher agent.
         This defines how the model should interpret the messages and what it should do.
@@ -53,28 +33,3 @@ class SearchAgent:
             MessagesPlaceholder(variable_name="messages"),
             ("system", "If you choose to call a tool, do so; otherwise, provide your findings in markdown text."),
         ])
-
-
-    def __create_agent(self):
-        """
-        Create the state graph for the researcher agent.
-        This defines the flow of the agent's operations.
-        """
-
-        graph = (
-            StateGraph(WorkerState)
-            .add_node("model", self.__invoke_model)
-            .add_node("tool", self.__tool_node)
-            .add_edge(START, "model")
-            .add_edge("tool", "model")
-            .add_conditional_edges(
-                "model",
-                should_continue,
-                {
-                    "tool": "tool",
-                    END: END,
-                }
-            )
-        )
-
-        return graph.compile()
---
+++ apps/agentic/core/tool_agent.py
@@ -0,0 +1,137 @@
+from abc import ABC, abstractmethod
+
+from langgraph.graph import StateGraph, START, END
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+
+from apps.agentic.core.messages import WorkerState
+from apps.agentic.core.utils import build_llm, should_continue
+
+import os
+import sys
+import numpy
+from datetime import datetime
+
+from lib import config
+from lib.plots import bar
+from lib.utils import generate_plot_file_name
+from lib.logger import get_logger
+
+logger = get_logger("YADA")
+
+
+class ToolAgent(ABC):
+
+    """
+    Abstract Base class for agents that use a language model to generate data and can call tools.
+    """
+
+    def __init__(self, tools, tool_node):
+        self.__tools = tools
+        self.__tool_node = tool_node
+        self.__llm = build_llm()
+        self.__prompt = self.create_prompt()
+        self.__tooled_llm = self.__llm.bind_tools(self.__tools)
+        self.__agent = self._create_agent()
+
+
+    @property
+    def tools(self):
+        """
+        Get the tools available to the agent.
+        """
+        
+        self.__tools
+
+    
+    @property
+    def tool_node(self):
+        """
+        Get the tool node used by the agent.
+        """
+
+        return self.__tool_node
+
+
+    @property
+    def agent(self):
+        """
+        Get the compiled agent state graph.
+        """
+
+        return self.__agent
+    
+
+    @property
+    def llm(self):
+        """
+        Get the language model used by the agent.
+        """
+
+        return self.__llm
+    
+
+    @property
+    def prompt(self):
+        """
+        Get the prompt template used by the agent.
+        """
+
+        return self.__prompt
+    
+    
+    @property
+    def tooled_llm(self):
+        """
+        Get the language model bound with tools.
+        """
+
+        return self.__tooled_llm
+    
+
+    async def _invoke_model(self, state: WorkerState, config=None) -> WorkerState:
+        """
+        Invoke the agent with the current state.
+        """
+
+        messages = state["messages"]
+        prompt_messages = self.prompt.format_messages(messages=messages)
+        result = await self.tooled_llm.ainvoke(prompt_messages)
+        return {"messages": [result]}
+
+    
+    def _create_agent(self):
+        """
+        Create the state graph for the time series plot agent.
+        This defines the flow of the agent's operations.
+        """
+
+        graph = (
+            StateGraph(WorkerState)
+            .add_node("model", self._invoke_model)
+            .add_node("tool", self.tool_node)
+            .add_edge(START, "model")
+            .add_edge("tool", "model")
+            .add_conditional_edges(
+                "model",
+                should_continue,
+                {
+                    "tool": "tool",
+                    END: END,
+                }
+            )
+        )
+
+        return graph.compile()
+    
+
+    @abstractmethod   
+    def create_prompt(self):
+        """
+        Create the prompt template for the agent.
+        This defines how the model should interpret the messages and what it should do.
+        """
+
+        raise NotImplementedError("Subclasses must implement this method to create a prompt template.") 
+    
